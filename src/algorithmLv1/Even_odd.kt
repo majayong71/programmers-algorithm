d6d54533ca39bf7caf00e422084d0b1f026cdecc
majayong71 <com4032647@gmail.com>
2025-04-29 22:17:55 +0900

    Lv.1 짝수와 홀수 ver 2

--- src/algorithmLv1/Even_odd.kt
+++ src/algorithmLv1/Even_odd.kt
@@ -2,15 +2,6 @@ package algorithmLv1
 
 class EvenOdd {
     fun solution(num: Int): String {
-
-        //        val isEven = true
-        //        val isOdd = false
-        //
-        //        if(isEven) {
-        //            println("짝수")
-        //        } else {
-        //            println("홀수")
-        //        }
         val isEven = num % 2 ==0
         return if(isEven) "짝수" else "홀수"
     }
