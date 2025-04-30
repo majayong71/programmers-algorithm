3e2c205542f7f6e95e524e861d83ebf13e9a1f6e
majayong71 <com4032647@gmail.com>
2025-04-30 21:12:28 +0900

    Lv.1 평균값 구하기.

---
+++ src/algorithmLv1/AverageCalculator.kt
@@ -0,0 +1,21 @@
+package AlgorithmLv1
+
+class AverageCalculator {
+
+}
+
+fun solution(arr: IntArray): Double {
+    val sum = arr.sum ()
+    return sum.toDouble() / arr.size
+}
+
+
+
+fun main() {
+    val input1 = intArrayOf(1,2,3,4)
+    val input2 = intArrayOf(5,5)
+
+    println(solution(input1))
+    println(solution(input2))
+
+}
