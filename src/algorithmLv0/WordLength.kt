9479b79c91dd53a062afa0ab57dd5e0f4e9307f9
majayong71 <com4032647@gmail.com>
2025-05-05 00:50:22 +0900

    Lv.0 단어 길이

---
+++ src/algorithmLv0/WordLength.kt
@@ -0,0 +1,13 @@
+package algorithmLv0
+
+class WordLengthCalculator {
+    fun run() {
+        val word = readLine()!!
+        println(word.length)
+    }
+}
+
+fun main() {
+    val calculator = WordLengthCalculator()
+    calculator.run()
+}
