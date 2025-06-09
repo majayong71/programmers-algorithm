438165dc08367801fb604480a66d79e35529145c
majayong71 <wotkd2647@naver.com>
2025-06-09 16:46:21 +0900

    기초 알고리즘 문제 풀이

--- src/algorithmLv0/DivisionCalculator.java
+++ src/algorithmLv0/DivisionCalculator.java
@@ -2,4 +2,16 @@ package algorithmLv0;
 
 public class DivisionCalculator {
 
+    public int solution (int num1, int num2) {
+
+        double result = (double) num1 / num2 * 1000;
+
+        return (int) result;
+    }
+
+    public static void main(String[] args) {
+        DivisionCalculator divisionCalculator = new DivisionCalculator();
+        double result = divisionCalculator.solution(1,16);
+        System.out.println(result);
+    }
 }
