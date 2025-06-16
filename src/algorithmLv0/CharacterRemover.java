5dc187478038171b24db3b27ee44deece28fe721
majayong71 <wotkd2647@naver.com>
2025-06-16 15:11:02 +0900

    특정 문자 제거 기능 구현

--- src/algorithmLv0/CharacterRemover.java
+++ src/algorithmLv0/CharacterRemover.java
@@ -2,4 +2,15 @@ package algorithmLv0;
 
 public class CharacterRemover {
 
+    public String solution(String my_string, String letter) {
+
+        String result = "";
+
+        for (int i=0; i < my_string.length(); i++) {
+            if (my_string.charAt(i) != letter.charAt(0)) {
+                result += my_string.charAt(i);
+            }
+        }
+        return result;
+    }
 }
